9b401f50eb114e99721c4fce10a11cd441ee760a
Cristian Suárez Vera <cristian.suarez.ulpgc@gmail.com>
2023-05-11 10:39:28 +0100

    vite setup

--- src/components/input/input.js
+++
@@ -1,7 +0,0 @@
-function InputText({id, placeholder, updateData}) {
-    return (
-        <input type="text" id={id} placeholder={placeholder} onChange={(e) => updateData(e.target.value)}/>
-    );
-}
-
-export default InputText;
