ac0515aed9e84d963a2574fd06287e9179623b2a
Cristian Suarez Vera <cristian@DESKTOP-A5IPOAM>
2023-05-17 21:50:16 +0100

    add set of questions

--- src/pages/track/TrackPage.tsx
+++ src/pages/track/TrackPage.tsx
@@ -1,21 +1,27 @@
-import {FormEvent, useState} from "react";
+import {FormEvent, useEffect, useState} from "react";
 import TextInput from "../../components/TextInput";
 import {CheckpointType, TeamType} from "../../services/getTeamTokenMock.tsx";
 import {getCurrentCheckpoint} from "../../services/getCurrentCheckpointMock.tsx";
 import {sendAnswer} from "../../services/sendAnswerMock.tsx";
+
+const emptyCheckpoint: CheckpointType = {question: "", answer: ""};
 
 type TrackProps = {
     team: TeamType,
 }
 export const TrackPage = ({team}: TrackProps) => {
     const [answer, setAnswer] = useState("");
-    const [checkpoint, setCheckpoint] = useState<CheckpointType>(getCurrentCheckpoint(team));
+    const [checkpoint, setCheckpoint] = useState<CheckpointType>(emptyCheckpoint);
+    useEffect(() => {
+        getCurrentCheckpoint(team).then(setCheckpoint).catch(console.error);
+    }, []);
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault(); // Prevents the page from reloading
         sendAnswer(team, answer);
-        const newCheckpoint = getCurrentCheckpoint(team);
-        setCheckpoint(newCheckpoint);
+        getCurrentCheckpoint(team)
+            .then(setCheckpoint)
+            .catch(console.error);
     }
 
     return (
--- src/services/getCurrentCheckpointMock.tsx
+++ src/services/getCurrentCheckpointMock.tsx
@@ -1,9 +1,22 @@
 import {CheckpointType, TeamType} from "./getTeamTokenMock.tsx";
 
-export const getCurrentCheckpoint = (team: TeamType): CheckpointType => {
+const questions: string[] = [
+  "¿Cuál es el nombre de la primera persona que llegó a la luna?",
+  "¿Como se llama el primer perro en el espacio?",
+  "¿Donde se encuentra el centro de control de la NASA?",
+  "¿Cuantos planetas hay en el sistema solar?",
+];
+
+let currentQuestion = 0;
+export const getCurrentCheckpoint = (team: TeamType): Promise<CheckpointType> => {
     console.log("get current checkpoint", team.leader);
-    return {
-        question: "my question",
-        answer: undefined
-    };
+    if (currentQuestion >= questions.length) currentQuestion = 0;
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve({
+                question: questions[currentQuestion++],
+                answer: ""
+            })
+        }, 2000);
+    });
 }
