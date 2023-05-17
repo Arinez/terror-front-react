eeed05d95bfdaf467ff999cffba602c2adf07f00
Cristian Suarez Vera <cristian@DESKTOP-A5IPOAM>
2023-05-17 22:23:01 +0100

    add loading component

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,21 +1,29 @@
-import {FormEvent, useState} from "react";
+import {Dispatch, FormEvent, useState} from "react";
 import TextInput from "../../components/TextInput";
 import {getTeamToken, TeamType} from "../../services/getTeamTokenMock.tsx";
 
 type LoginPageProps = {
-    useTeam: (team: TeamType) => void,
+    useTeam: Dispatch<TeamType>,
+    setLoading: Dispatch<boolean>,
 }
 
-export const LoginPage = ({useTeam}: LoginPageProps) => {
+export const LoginPage = ({useTeam, setLoading}: LoginPageProps) => {
     const [leader, setLeader] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault(); // Prevents the page from reloading
-        const response = getTeamToken(leader, password);
-        if (response.token !== "incorrect"){
-            useTeam(response);
-        }
+        setLoading(true);
+        getTeamToken(leader, password)
+            .then(response => {
+                if (response.token !== "incorrect") {
+                    useTeam(response); // FIXME: React Hook "useTeam" cannot be called inside a callback.
+                }else {
+                    console.error("Credentials incorrect");
+                }
+            })
+            .catch(console.error)
+            .finally(() => setLoading(false));
     };
 
     return (
--- src/services/getTeamTokenMock.tsx
+++ src/services/getTeamTokenMock.tsx
@@ -8,13 +8,20 @@ export type TeamType = { // TODO: move to isolate type file
     token: string;
 }
 
-export const getTeamToken = (leader: string, password: string): TeamType => {
-    if (leader === "admin" && password === "admin") {
-        return {leader, token: "admin token"};
-    }
-    if (leader === "cris") {
-        return {leader, token:"user token"}
-    }
+export const getTeamToken = (leader: string, password: string):Promise<TeamType> => {
+    console.log("get team token", leader, password);
 
-    return {leader, token: "incorrect"};
+    return new Promise(resolve => {
+        setTimeout(() => {
+            if (leader === "admin" && password === "admin") {
+                resolve({leader, token: "admin token"});
+            }
+            if (leader === "cris") {
+                resolve({leader, token:"user token"});
+            }
+
+            resolve({leader, token: "incorrect"});
+
+        }, 2000);
+    });
 }
