b2d526909f9d52766b979612699a950426da4ef2
Cristian Suarez Vera <cristian@DESKTOP-A5IPOAM>
2023-05-22 20:10:11 +0100

    set up local storage for team

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import './App.css'
 import {LoginPage} from './pages/login/LoginPage.tsx'
 import {TeamType} from './services/getTeamTokenMock.tsx'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {TrackPage} from "./pages/track/TrackPage.tsx";
 import Loading from "./components/Loading.tsx";
 
 function App() {
     const [team, setTeam] = useState<TeamType>()
     const [loading, setLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+        const team = localStorage.getItem("team");
+        if (team !== null && team !== "undefined") {
+            setTeam(JSON.parse(team))
+        }
+    }, [])
+
+    useEffect(() => {
+        if (team !== undefined) {
+            window.localStorage.setItem("team", JSON.stringify(team))
+        }
+    }, [team])
 
     if (loading) return (<Loading text={"Cargando..."}/>)
 
