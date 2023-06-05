ef42ab3b2de3c9a0f51ae596c377f679fff672b3
Cristian Suarez Vera <cristian@DESKTOP-A5IPOAM>
2023-06-05 19:31:21 +0100

    style: tracking page

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -4,16 +4,28 @@ type TextInputProps = {
     id: string,
     placeholder: string,
     onChange: (value: string) => void,
+    maxLength?: number
 }
 
-function TextInput({id, placeholder, onChange}: TextInputProps) {
+function TextInput({id, placeholder, onChange, maxLength}: TextInputProps) {
 
     function handleChange(e: ChangeEvent<HTMLInputElement>) {
         onChange(e.target.value);
     }
 
+    if (maxLength) {
+        return (
+            <input type="text" id={id}
+                   placeholder={placeholder}
+                   onChange={handleChange}
+                   maxLength={maxLength}/>
+        )
+    }
+
     return (
-        <input type="text" id={id} placeholder={placeholder} onChange={handleChange}/>
+        <input type="text" id={id}
+               placeholder={placeholder}
+               onChange={handleChange}/>
     )
 }
 
