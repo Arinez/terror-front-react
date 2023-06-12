cfc38bd7fd0b742a1a60265b74a202505a9fd016
Cristian Suarez Vera <cristian@DESKTOP-A5IPOAM>
2023-06-12 23:01:26 +0100

    add multiple answer types

--- src/pages/track/TrackPage.tsx
+++ src/pages/track/TrackPage.tsx
@@ -28,6 +28,7 @@ type TrackProps = {
 }
 
 export const TrackPage = ({team, storeTrack}: TrackProps) => {
+    // TODO: everytime the answer changes the page is rendered again, this is not good
     const [answer, setAnswer] = useState("");
     const [checkpoint, setCheckpoint] = useState<Checkpoint>(emptyCheckpoint);
     const [track, setTrack] = useState<Track>(emptyTrack);
@@ -58,15 +59,7 @@ export const TrackPage = ({team, storeTrack}: TrackProps) => {
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault(); // Prevents the page from reloading
-        if (answer.length !== 4) return; // TODO: Show error message, this is not always 4
-        setLoading(true);
-        let newTrack = updateTrackAnswer(track, answer);
-        if (!isFinalCheckpoint(track)) {
-            newTrack = updateTrackStep(newTrack);
-            sendAnswer(team, answer, track.steps[track.currentStep].id);
-        }
-        setTrack(newTrack)
-        setLoading(false);
+        sendAnswerComponent(answer)
     }
 
     if (loading) return (<Loading text={"Cargando..."}/>)
@@ -77,13 +70,65 @@ export const TrackPage = ({team, storeTrack}: TrackProps) => {
         </>
     )
 
+    // TODO: rename this function
+    function sendAnswerComponent(answerComponent: string) {
+        if (answerComponent.length < 4) {
+            console.error("Answer is not 4 characters long")
+            return;
+        } // TODO: Show error message, this is not always 4
+        setLoading(true);
+        let newTrack = updateTrackAnswer(track, answerComponent);
+        if (!isFinalCheckpoint(track)) {
+            newTrack = updateTrackStep(newTrack);
+            sendAnswer(team, answerComponent, track.steps[track.currentStep].id);
+        }
+        setTrack(newTrack)
+        setAnswer("")
+        setLoading(false);
+    }
+
+    function sendOption(e: FormEvent) {
+        e.preventDefault(); // Prevents the page from reloading
+        if (!document.querySelector('input[name="answer"]:checked')) return; // TODO: Show error message, answer not selected
+        const answer = (document.querySelector('input[name="answer"]:checked') as HTMLInputElement).value;
+        sendAnswerComponent(answer)
+    }
+
+    console.log(checkpoint)
+    if (checkpoint.answerType === "OPTION") return (
+        <>
+            <h1>{checkpoint.title}</h1>
+            {checkpoint.images.length > 0 && checkpoint.images.map(image => <img src={image}/>)}
+            <form onSubmit={sendOption}>
+                { checkpoint.question.split("|").map(line => {
+                    return (
+                        <>
+                            <input
+                                type="radio"
+                                value={line}
+                                id={line}
+                                name="answer"
+                            />
+                            <label htmlFor={line}>{line}</label>
+                            <br/>
+                        </>
+                    );
+                }) }
+                <br/>
+                <button type="submit">Enviar</button>
+            </form>
+        </>
+    );
+
+    // TODO: split all this logic into components
     return (
         <>
             <h1>{checkpoint.title}</h1>
-            <p>{checkpoint.question}</p>
-            { checkpoint.images.length > 0 && checkpoint.images.map(image => <img src={image}/> ) }
+            { checkpoint.answerType !== "OPTION" && <p>{checkpoint.question}</p> }
+            { checkpoint.images.length > 0 && checkpoint.images.map(image => <img src={image} key={image}/> ) }
             <form onSubmit={handleSubmit}>
-                <TextInput id="answer" placeholder="- - - -" onChange={setAnswer} maxLength={4}/>
+                { checkpoint.answerType === "4_CHAR" && <TextInput id="answer" placeholder="- - - -" onChange={setAnswer} maxLength={4}/> }
+                { checkpoint.answerType === "TEXT" && <TextInput id="answer" placeholder="respuesta" onChange={setAnswer}/> }
                 <br/>
                 <button type="submit">Enviar</button>
             </form>
